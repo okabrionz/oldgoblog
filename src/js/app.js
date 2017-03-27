3b1c1c4eccc6080105e953c5d9b58c230088eb0d
Oka bRionZ <okabrionz@gmail.com>
2017-03-27 21:44:49 +0700

    remove tinify

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,1 @@
 // JS Goes here - ES6 supported
-var tinify = require("tinify");
-tinify.key = "ixsCwR9siKeL1nyGj8iH427odcoExWFR";
-var source = tinify.fromFile("site/static/img");
-source.toFile("img");
