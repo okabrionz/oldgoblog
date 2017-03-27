5c8651a1469dff53b65cb7013a53027b71fe453d
Oka bRionZ <okabrionz@gmail.com>
2017-03-27 21:31:08 +0700

    added tinify

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,5 @@
 // JS Goes here - ES6 supported
+var tinify = require("tinify");
+tinify.key = "ixsCwR9siKeL1nyGj8iH427odcoExWFR";
+var source = tinify.fromFile("site/static/img");
+source.toFile("img");
